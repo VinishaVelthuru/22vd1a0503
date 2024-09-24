179c8f4449c9a9ab2414125316e6c99b72ba70c6
VinishaVelthuru <45623617+VinishaVelthuru@users.noreply.github.com>
2024-09-24 12:12:50 +0530

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,10 @@
+<?php
+echo "your name is";
+echo $-POST["name"];
+echo"<br>";
+echo"your email is";
+echo $-POST["email"];
+echo"<br>";
+echo"your password is";
+echo $-POST<"passward"];
+echo"<br>";
